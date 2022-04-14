04cfb39c4618be206b89008a045c1c7994a64054
HealthyPlusJJCEK <healthyplusc171@gmail.com>
2022-04-14 14:56:34 -0500

    bnm

--- src/app/modules/versucursales/versucursales.component.ts
+++ src/app/modules/versucursales/versucursales.component.ts
@@ -30,7 +30,6 @@ export class VersucursalesComponent implements OnInit {
               private title: Title) { }
 
   ngOnInit(): void {
-    this.title.setTitle("Ver sucursales")
     if(JSON.parse(sessionStorage['user']).length!=""){
       this.listarSucursales()
     }else {
